a2089067dd6b17206ea0dcddb849552e4a4bd5c7
ustleseanbruneau <ustbrun9912@gmail.com>
2025-05-21 08:06:59 -0500

    L22-Adding User Input to React Form

---
+++ src/components/NewTodoForm.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+
+function NewTodoForm() {
+
+  const [description, setDescription] = useState('');
+  const [assigned, setAssigned] = useState('');
+
+//  const descriptionChange = (event) => {
+//    console.log('description', event.target.value);
+//    setDescription(event.target.value);
+//  }
+
+//  const assignedChange = (event) => {
+//    console.log('assigned', event.target.value);
+//    setAssigned(event.target.value);
+//  }
+
+  return (
+    <div className="mt-5">
+      <form>
+        <div className='mb-3'>
+          <label className="form-label">Assigned</label>
+          <input 
+            type="text" 
+            className="form-control" 
+            required 
+            onChange={e => setAssigned(e.target.value)} 
+            value={assigned} 
+          />
+        </div>
+        <div className='mb-3'>
+          <label className="form-label">Description</label>
+          <textarea 
+            className="form-control" 
+            rows={3} 
+            required 
+            onChange={e => setDescription(e.target.value)} 
+            value={description} 
+          />
+        </div>
+        <button type='button' className='btn btn-primary mt-3'>Add Todo</button>
+      </form>
+    </div>
+  )
+}
+
+
+// onChange={assignedChange} change to onChange={e => setAssigned(e.target.value)} 
+// onChange={descriptionChange} change to onChange={e => setDescription(e.target.value)}
+
+export default NewTodoForm;
