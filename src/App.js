0ce1f315013e9f98bdcc448faef516aa634dbc15
ustleseanbruneau <ustbrun9912@gmail.com>
2025-05-21 06:55:46 -0500

    L19-React onClick Listeners

--- src/App.js
+++ src/App.js
@@ -10,6 +10,19 @@ function App() {
     {rowNumber: 4, rowDescription: 'Charge Phone Battery', rowAssigned: 'User One'}
   ];
 
+  const addTodo = () => {
+    //console.log('Our addTodo btn has been clicked');  
+    if (todos.length > 0) {
+      const newTodo = {
+        rowNumber: todos.length + 1,
+        rowDescription: 'New Todo',
+        rowAssigned: 'User Three'
+      };
+      todos.push(newTodo);
+      console.log(todos);
+    }
+  }
+
   return (
     <div className='mt-5 container'>
       <div className="card">
@@ -18,10 +31,17 @@ function App() {
         </div>
         <div className="card-body">
           <TodoTable todos={todos} />
+          <button className='btn btn-primary' onClick={addTodo}>
+            Add New Todo
+          </button>
         </div>
       </div>
     </div>
   );
 }
+
+// Note about button onClick
+// onClick={addTodo} => without () means we are passing the function reference  (function will be called when the button is clicked)
+// onClick={addTodo()} => with () means we are calling the function immediately (function will be called when the component is rendered, not when the button is clicked)
 
 export default App;
